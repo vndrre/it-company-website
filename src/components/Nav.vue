6284852af6fbd4afd66eaf31246980dd3dee000f
AndreLp5 <aleppik7@gmail.com>
2024-10-17 11:27:35 +0300

    intialize vue project

---
+++ src/components/Nav.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+    
+<template>
+    <div class="flex justify-between items-center px-[150px] py-[30px] font-main bg-white">
+        <div>
+            <a href="/" class="text-3xl font-semibold">Logo</a>
+        </div>
+
+        <ul class="flex items-center gap-10 text-xl">
+            <li>
+                <a href="/" class="hover:font-medium duration-100">About</a>
+            </li>
+
+            <li>
+                <a href="/" class="hover:font-medium duration-100">Our Services</a>
+            </li>
+
+            <li>
+                <a href="/" class="hover:font-medium duration-100">Portfolio</a>
+            </li>
+
+            <li>
+                <a href="/" class="hover:font-medium duration-100">Portfolio</a>
+            </li>
+
+            <li>
+                <a href="/" class="hover:font-medium duration-100">Blog</a>
+            </li>
+
+            <li>
+                <a href="/" class="hover:font-medium duration-100">Contact</a>
+            </li>
+
+            <a href="/" class="bg-[#FF3E54] hover:bg-[#f13a4f] duratio-100 text-white rounded-md p-3 font-semibold ml-2" >Get In Touch</a>
+        </ul>
+    </div>
+</template>
