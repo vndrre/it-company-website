b96d7374a44e43a0315614fef95be85835ecd260
AndreLp5 <aleppik7@gmail.com>
2024-10-17 12:10:33 +0300

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
   import Nav from './components/Nav.vue';
+  import HomePage from './components/views/HomePage.vue';
 </script>
 
 <template>
   <Nav />
+  <HomePage />
 </template>
 
---
+++ src/components/views/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="grid place-items-center px-[100px] py-[50px] font-main">
+        <div class="flex items-center">
+            <div class="grid grid-cols-1 gap-3">
+                <p class="text-[#FF3E54] text-[24px] font-semibold">\ We Are Here \</p>
+                <h1></h1>
+            </div>
+
+            <div>
+                <img src="/src/assets/landing-image.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
